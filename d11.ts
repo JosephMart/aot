d735640afe485f3c61001345fdbd5921bd992e69
Joseph Martinsen <josmarti@adobe.com>
2023-12-12 13:26:19 -0800

    d11

--- d11.ts
+++ d11.ts
@@ -1,1 +1,17 @@
-// TODO
+type DeepReadonlyArray<
+    Cur extends ReadonlyArray<unknown>,
+    Acc extends ReadonlyArray<unknown> = []
+> = Cur["length"] extends Acc["length"]
+    ? Acc
+    : DeepReadonlyArray<
+          Cur,
+          readonly [...Acc, SantaListProtector<Cur[Acc["length"]]>]
+      >;
+
+type SantaListProtector<T> = T extends Record<string, unknown>
+    ? {
+          readonly [Prop in keyof T]: SantaListProtector<T[Prop]>;
+      }
+    : T extends unknown[]
+    ? DeepReadonlyArray<T>
+    : T;
