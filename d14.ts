d8e432c84960e7490986e3315f6cdab8012dae95
Joseph Martinsen <josmarti@adobe.com>
2023-12-14 07:53:21 -0800

    tweak

--- d14.ts
+++ d14.ts
@@ -1,6 +1,6 @@
 type Parse<
     Base extends string,
-    Delimiter extends string = "/"
+    Delimiter extends string
 > = Base extends `${infer Pre}${Delimiter}${infer Post}`
     ? Post extends string
         ? [Pre, ...Parse<Post, Delimiter>]
