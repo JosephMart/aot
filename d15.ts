c4ddb0cf7cd4091520553ffa19fa392fab316581
Joseph Martinsen <josmarti@adobe.com>
2023-12-14 21:11:00 -0800

    d15

---
+++ d15.ts
@@ -0,0 +1,11 @@
+type BoxToysExpand<
+    Toy extends string,
+    End extends number,
+    Acc extends Toy[] = []
+> = Acc["length"] extends End
+    ? [...Acc]
+    : BoxToysExpand<Toy, End, [...Acc, Toy]>;
+
+type BoxToys<Toy extends string, End extends number> = {
+    [Prop in End]: BoxToysExpand<Toy, Prop>;
+}[End];
