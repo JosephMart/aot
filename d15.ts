50f1a69bb756b2acb1682a192d0efa31ff004f68
Joseph Martinsen <josmarti@adobe.com>
2023-12-16 08:28:11 -0800

    simplify d15

--- d15.ts
+++ d15.ts
@@ -2,9 +2,7 @@ type BoxToysExpand<
     Toy extends string,
     End extends number,
     Acc extends Toy[] = []
-> = Acc["length"] extends End
-    ? [...Acc]
-    : BoxToysExpand<Toy, End, [...Acc, Toy]>;
+> = Acc["length"] extends End ? Acc : BoxToysExpand<Toy, End, [...Acc, Toy]>;
 
 type BoxToys<Toy extends string, End extends number> = {
     [Prop in End]: BoxToysExpand<Toy, Prop>;
