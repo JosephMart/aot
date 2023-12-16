3b2d977b770585454822d4c5e4c46e8dbb785bd0
Joseph Martinsen <josmarti@adobe.com>
2023-12-16 08:25:40 -0800

    d16

---
+++ d16.ts
@@ -0,0 +1,14 @@
+type FindSantaRow<Row> = Row extends [...infer A, infer B]
+    ? B extends "🎅🏼"
+        ? A["length"]
+        : FindSantaRow<A>
+    : never;
+
+type FindSanta<Matrix extends unknown[]> = {
+    [Prop in keyof Matrix]: FindSantaRow<Matrix[Prop]> extends never
+        ? never
+        : [
+              Prop extends `${infer Num extends number}` ? Num : never,
+              FindSantaRow<Matrix[Prop]>
+          ];
+}[number];
